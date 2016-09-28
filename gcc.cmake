fce7c7441224fa316fb5acc55b74b9427b406b48
Alexhuszagh <ahuszagh@gmail.com>
2016-09-28 00:29:19 -0500

    New toolchain definitions

---
+++ gcc.cmake
@@ -0,0 +1,7 @@
+#  :copyright: (c) 2015-2016 The Regents of the University of California.
+#  :license: MIT, see LICENSE.md for more details.
+
+# COMPILERS
+# ---------
+SET(CMAKE_C_COMPILER gcc)
+SET(CMAKE_CXX_COMPILER g++)
