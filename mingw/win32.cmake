41fce701b1e16cb747ece97df084efbfd500205d
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-04-03 19:03:27 -0700

    Bug fix for the proper AR when using Fedora MinGW

--- mingw/win32.cmake
+++ mingw/win32.cmake
@@ -5,6 +5,7 @@ SET(CMAKE_SYSTEM_NAME Windows)
 
 # COMPILERS
 # ---------
+set(CMAKE_AR i686-w64-mingw32-ar)
 SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
 SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
 SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
