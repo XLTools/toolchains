08ed1d110fbd6ba81eeae2795c692eae5fcdb642
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2017-06-09 21:03:00 -0700

    Added darwin toolchains

---
+++ clang_linux64.cmake
@@ -0,0 +1,11 @@
+#  :copyright: (c) 2015-2016 The Regents of the University of California.
+#  :license: MIT, see LICENSE.md for more details.
+
+# COMPILERS
+# ---------
+SET(CMAKE_C_COMPILER clang)
+SET(CMAKE_CXX_COMPILER clang++)
+
+set(CMAKE_C_FLAGS -m64)
+set(CMAKE_CXX_FLAGS -m64)
+set(CMAKE_LINK_FLAGS -m64)
